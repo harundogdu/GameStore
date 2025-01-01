fde5d8f2497db739fb24159498abfb4df36cc215
Harun Doğdu <harundogdu06@gmail.com>
2025-01-01 16:33:29 +0300

    Implement game listing and retrieval endpoints in GameStore API

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,62 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new(
+        1,
+        "The Legend of Zelda: Breath of the Wild",
+        "Action-adventure",
+        59.99m,
+        new DateOnly(2017, 3, 3)
+    ),
+    new(
+        2,
+        "Super Mario Odyssey",
+        "Platformer",
+        59.99m,
+        new DateOnly(2017, 10, 27)
+    ),
+    new(
+        3,
+        "Mario Kart 8 Deluxe",
+        "Racing",
+        59.99m,
+        new DateOnly(2017, 4, 28)
+    ),
+    new(
+        4,
+        "Splatoon 2",
+        "Shooter",
+        59.99m,
+        new DateOnly(2017, 7, 21)
+    ),
+    new(
+        5,
+        "Animal Crossing: New Horizons",
+        "Life simulation",
+        59.99m,
+        new DateOnly(2020, 3, 20)
+    ),
+    new(
+        6,
+        "Pokémon Sword and Shield",
+        "Role-playing",
+        59.99m,
+        new DateOnly(2019, 11, 15)
+    ),
+];
+
+// GET: Get all games
+app.MapGet("games",() => games);
+
+// GET: Get a game by id
+app.MapGet("games/{id}", (int id) => {
+    return games.Find(item => item.Id == id);
+});
+
+// GET: Get Main page
+app.MapGet("/", () => "Hello from GameStore.Api");
 
 app.Run();
