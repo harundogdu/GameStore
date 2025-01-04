255b4d3ee8b79bcada63e3a54ed23fb49184f04f
Harun Doğdu <harundogdu06@gmail.com>
2025-01-05 01:03:00 +0300

    Seed initial genre data in GameStoreContext

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -7,4 +7,17 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbCo
 {
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Fighting" },
+            new { Id = 2, Name = "Shooter" },
+            new { Id = 3, Name = "RPG" },
+            new { Id = 4, Name = "Sports" },
+            new { Id = 5, Name = "Platformer" },
+            new { Id = 6, Name = "Puzzle" },
+            new { Id = 7, Name = "Racing" },
+            new { Id = 8, Name = "Strategy" }
+        );
+    }
 }
