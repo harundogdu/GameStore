2d8ec2176010e9d09986cb2e5ea399cfdc042115
Harun Doğdu <harundogdu06@gmail.com>
2025-01-04 23:27:13 +0300

    Update SQLite connection string to use configuration settings

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,7 +3,7 @@ using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = "Data Source=GameStore.db";
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connectionString);
 
 var application = builder.Build();
