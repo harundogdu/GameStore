c41091805949826d4fc54b520b5a8d701b870865
Harun Doğdu <harundogdu06@gmail.com>
2025-01-04 22:49:52 +0300

    Add SQLite database connection to GameStore.Api

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,11 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
 var application = builder.Build();
 
 application.MapGamesEndpoints();
